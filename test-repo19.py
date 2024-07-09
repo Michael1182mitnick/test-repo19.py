3db6de4b565de695583bc9db8c8c429684b23f15
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-07-08 22:35:43 -0800

    Create test-repo19.py

---
+++ test-repo19.py
@@ -0,0 +1,9 @@
+# calculate the area of a triangle
+
+height = int(input("Enter the height: "))
+base = int(input("Enter the base: "))
+
+# To calculate the area
+area = (height*base) * 1/2
+
+print("\nThe are of a Triangle: ", float(area))
